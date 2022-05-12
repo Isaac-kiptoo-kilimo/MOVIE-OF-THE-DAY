efe2114c48a6ae76bd544654f2db74516bd68081
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-12 08:47:48 +0300

    feature:added the config file,requirements and init files

---
+++ start.sh
@@ -0,0 +1,9 @@
+
+export MOVIE_API_KEY=''
+export SECRET_KEY=''
+ SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://moringa:isaac@localhost/watchlist'
+export MAIL_USERNAME=''
+export MAIL_PASSWORD=''
+
+
+python3.8 manage.py server
