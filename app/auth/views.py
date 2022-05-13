1864c25f9c3043c4c908f0e9548e0a285776bbf4
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-13 10:47:48 +0300

    Adding on log in form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,12 @@
 
 from unicodedata import category
-from flask import render_template,url_for,redirect,flash
+from flask import render_template,url_for,redirect,flash,request
 from . import main
 from .forms import RegistrationForm,LoginForm,PitchForm
 from models import Pitch, User,Category
 from app import db
-
-
+from . import auth
+from flask_login import login_user,logout_user,login_required
 
 
 @main.route('/register',methods=['GET','POST'])
@@ -28,4 +28,29 @@ def register():
     flash('Username is taken')
     print(form.username.data,form.email.data,form.password.data,form.confirm_password.data)
     return render_template('register.html',title='register',form=form)
+
+#diana
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "movie of the day login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
+                           
+                           
+#logout
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
 
