d48ce79e82f3eb98ec8a01443414bcc1d6a94348
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-13 06:29:28 +0300

    feature:added the models

--- app/request.py
+++ app/request.py
@@ -1,4 +1,5 @@
 import urllib.request,json
+from .models import Movie
 
 #Get api key
 api_key = None
@@ -11,16 +12,16 @@ def configure_request(app):
     api_key = app.config['MOVIE_API_KEY']
     base_url = app.config['MOVIE_API_BASE_URL']
 
-def get_movies():
-    get_movies_url = base_url.format(api_key)
+def get_movies(category):
+    get_movies_url = base_url.format(category,api_key)
 
     with urllib.request.urlopen(get_movies_url) as url:
         get_movies_data = url.data()
         get_movies_respone = json.loads(get_movies_data)
 
         movies_results = None
-        if get_movies_respone['category']:
-            get_movies_list = get_movies_respone['category']
+        if get_movies_respone['results']:
+            get_movies_list = get_movies_respone['results']
             movies_results = process(get_movies_list)
     return movies_results
 
@@ -31,10 +32,15 @@ def process(movies_list):
         title = movies_item.get('original_title')
         overview = movies_item.get('overview')
         poster = movies_item.get('poster_path')
+        genres=movies_item.get('genres')
+        origin_country=movies_item.get('origin_country')
+        origin_language=movies_item.get('origin_language')
         vote_average = movies_item.get('vote_average')
         vote_count = movies_item.get('vote_count')
-
-        movies_object = (id,title,overview,poster,vote_average,vote_count)
+        
+        
+        movies_object =Movie(id,title,overview,poster,
+        genres,origin_country,origin_language,vote_average,vote_count)
         movies_results.append(movies_object)
     return movies_results
         
